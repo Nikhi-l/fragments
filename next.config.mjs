939eaf4ced9fbd75b3b32ab256df31886baf5f84
Nikhil gupta <gnikhil335@gmail.com>
2025-07-01 03:35:20 +0530

    Fix Next.js image configuration for Pexels

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,14 @@ const nextConfig = {
     swcMinify: true
   },
   images: {
-    domains: ['images.pexels.com']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   }
 }
 
